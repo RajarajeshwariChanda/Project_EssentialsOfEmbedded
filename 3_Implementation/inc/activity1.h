314a3188282a821dbc00e100311bcda1774d9776
RajarajeshwariChanda <89605184+RajarajeshwariChanda@users.noreply.github.com>
2021-09-16 23:43:24 +0530

    Update activity1.h

--- 3_Implementation/inc/activity1.h
+++ 3_Implementation/inc/activity1.h
@@ -1,15 +1,10 @@
-#ifndef ACTIVITY1_H_INCLUDED
-#define ACTIVITY1_H_INCLUDED
+#ifndef __ACTIVITY1_H_
+#define __ACTIVITY1_H_
 
+ 
 #include <avr/io.h>
-#include<util/delay.h>
 
-#define SEAT_CHECK !(PIND &(1<<PD1))
-#define HEATER_ON (PIND &(1<<PD2))
-#define HEATER_OFF PORTD&=~(1<<PD2)
-#define LED_ON PORTD|=(1<<PD0)
-#define LED_OFF PORTD &=~(1<<PD0)
+void led();
 
-void SEAT_CHECKING();
 
-#endif
+#endif /** __ACTIVITY1_ */
